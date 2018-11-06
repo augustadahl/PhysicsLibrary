f2fb025ed99ef8374609f571eff6f7f0d8c5c425
augustadahl <august.adahl@outlook.com>
2018-11-06 21:14:18 +0100

    några fler medtoder med java doc

--- src/metoder/MethodLibrary.java
+++ src/metoder/MethodLibrary.java
@@ -14,10 +14,16 @@ public class MethodLibrary {
 		System.out.println(kelvinToCelsius(0));
 
 		System.out.println(fluidPressure(FluidTable.WATER, 10));
-		
+
 		System.out.println(pressureUnderWater(10));
+
+		System.out.println(kineticEnergy(2, 2));
 		
-		System.out.println(kineticEnergy(2,2));
+		System.out.println(potentialEnergy(2, 5));
+		
+		
+		
+		System.out.println(delta(5, 1));
 
 	}
 
@@ -45,41 +51,62 @@ public class MethodLibrary {
 	 * Denna metod räknar ut trycket i en vätska på ett visst djup.
 	 * 
 	 * @param fluid Vätskans densitet
-	 * @param deep Djupet i meter
-	 * @return Tryck på det bestämda djupet
+	 * @param deep  Djupet i meter
+	 * @return Tryck på det bestämda djupet Pascal??
 	 */
 	public static double fluidPressure(FluidTable fluid, double deep) {
 		double pressure = fluid.density * G * deep;
 		return pressure;
 	}
-	
+
 	/**
 	 * Denna metod räknar ut trycket i vatten på ett visst djup.
 	 * 
 	 * @param deep Djupet i meter
-	 * @return Tryck på det bestämda djupet
+	 * @return Tryck på det bestämda djupet pascal??
 	 */
 	public static double pressureUnderWater(double deep) {
 		return deep * G * 0.998 * 1E3;
 	}
 
 	/**
-	 * Denna metod beräknar Kinetisk energi på ett föremål 
-	 * @param mass Föremålets massa
-	 * @param velocity Hastigheten på föremålet
-	 * @return Kinetisk energi för föremålet
+	 * Denna metod beräknar Kinetisk energi på ett föremål
+	 * 
+	 * @param mass     Föremålets massa kg
+	 * @param velocity Hastigheten på föremålet m/s
+	 * @return Kinetisk energi för föremålet joule
 	 */
 	public static double kineticEnergy(double mass, double velocity) {
 		return (mass * velocity * velocity) / 2;
 	}
-	
-	public static double potentialEnergy(double mass, double height) {
-		
-	
-	}
-	//potentialEnergy(2,5) => 98.2
 
+	/**
+	 * Denna metod beräknar Lägesenergin för ett föremål
+	 * 
+	 * @param mass   Föremålets massa kg
+	 * @param height Höjden föremålet befinner sig på m
+	 * @return Lägesenergin för föremålet vid denna höjd joule
+	 */
+	public static double potentialEnergy(double mass, double height) {
+		return mass * height * G;
+	}
 	
+//	/**
+//	 * 
+//	 * @param height
+//	 * @return
+//	 */
+//	public static double fallSpeed(double height) {
+//		for(int i = height; i <= 0; i--) {
+//			
+//		}
+//	}
+//	//fallSpeed(2.5) => 7,00713922
 	
-	
+	public static double delta(double first, double last) {
+		return last - first;
+	}
+//	delta(1,10) => 9
+//	delta(5,1) => -4
+
 }
